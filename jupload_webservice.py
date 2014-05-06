bd8acd7a6b06152cb1fe5d730340e3571e46b625
avdata99 <andres@data99.com.ar>
2014-05-06 15:42:33 -0300

    new uploader for declaring webservices

---
+++ jupload_webservice.py
@@ -0,0 +1,55 @@
+from __future__ import with_statement
+import sys
+import urllib2
+import csv
+import settings
+from MultipartPostHandler import *
+
+def UnicodeDictReader(utf8_data, **kwargs):
+    csv_reader = csv.DictReader(utf8_data, **kwargs)
+    for row in csv_reader:
+        yield dict([(key, unicode(value, 'utf-8')) for key, value
+                    in row.iteritems()])
+
+def post_record(r):
+    print "uploading %s" % r.get('title').encode('utf-8')
+
+    form = dict(r)
+    form['auth_key'] = settings.auth_key
+
+    try:
+        print "Addind data [%s] file for %s" % (file_data.name, file_name)
+
+        import pprint
+        pp = pprint.PrettyPrinter(indent=4)
+        print "SENDING ..."
+        pp.pprint(form)
+
+        opener = urllib2.build_opener(MultipartPostHandler())
+        response = opener.open(settings.url_webservice, form)
+
+        print response.read()
+    except Exception, e:
+        print e
+    finally:
+        file_data.close()
+
+
+if __name__=="__main__":
+    csv_name=sys.argv[1]
+
+    csv_data = open(csv_name,"rb")
+    try:
+        for r in UnicodeDictReader(csv_data):
+            post_record(r)
+    finally:
+        csv_data.close()
+"""
+
+CSV sample
+
+title,description,category,tags,endpoint,impl_type,path_to_data,path_to_headers,token,algorithm,signature
+Webservice Test01 title,Webservice description text,Test,"tag01, tag02",http://api_enpoint.com,14,data,header,token,sign_algoritm,sign_name
+
+
+"""
--- settings.py.tpl
+++ settings.py.tpl
@@ -1,2 +1,8 @@
+
 auth_key="yourkeyhere"
+
+# http://your_api_domain/datastreams/publish
 url="http://junar.com/datastreams/publish"
+
+# http://your_api_domain/datastreams/publish_webservice
+url_webservice="http://junar.com/datastreams/publish_webservice"
